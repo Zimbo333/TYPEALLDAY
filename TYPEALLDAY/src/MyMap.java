0c1b39c229ebd64e7eec587b726d25d488e8bb9f
61070071 <61070071@kmitl.ac.th>
2019-11-20 20:24:54 +0700

    deen

--- TYPEALLDAY/src/MyMap.java
+++ TYPEALLDAY/src/MyMap.java
@@ -1,13 +1,19 @@
 
+import java.awt.BorderLayout;
+import static java.awt.BorderLayout.SOUTH;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.FlowLayout;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GraphicsDevice;
 import java.awt.GraphicsEnvironment;
+import java.awt.GridLayout;
 import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.text.AttributedCharacterIterator;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JDesktopPane;
@@ -22,16 +28,21 @@ public class MyMap extends JPanel implements KeyListener {
     static Toolkit tk = Toolkit.getDefaultToolkit();
     static int xSize = ((int) tk.getScreenSize().getWidth());
     static int ySize = ((int) tk.getScreenSize().getHeight());
-    ImageIcon ic = new   ImageIcon("C:\\Users\\chanl\\Desktop\\TYPEALLDAY\\TYPEALLDAY\\src\\images\\tenor.gif");
+    static JLabel lb = new JLabel();
+    static String txt = "";
     JDesktopPane dp = new JDesktopPane();
-    JLabel lbl = new JLabel(ic);
     JPanel transparentPanel = new JPanel();
     public static void main(String[] args) {
         MyMap p = new MyMap();
         JFrame fr = new JFrame();
         fr.setSize((xSize/2)+300, (ySize/2)+200);
+        lb.setFont(new Font("Dialog", Font.BOLD, 70));
+        p.add(lb);
         fr.add(p);
+        
         fr.addKeyListener(p);
+        
+        
         fr.setVisible(true);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         fr.setResizable(false);
@@ -45,6 +56,7 @@ public class MyMap extends JPanel implements KeyListener {
         g.drawImage(bg, 0, 0, this);
         g.drawImage(oong_b, 900,370, this);
         g.drawImage(oong_w, 0, 380, this);
+//        g.drawString((AttributedCharacterIterator) lb, 430, 500);
         
         
     }
@@ -53,16 +65,12 @@ public class MyMap extends JPanel implements KeyListener {
     }
 
     public void keyPressed(KeyEvent ke) {
-        if (ke.getKeyCode() == 37) { //Left
-            this.px -= 10;
-        } else if (ke.getKeyCode() == 38) { //Up
-            this.py -= 10;
-        } else if (ke.getKeyCode() == 39) { //Right
-            this.px += 10;
-        } else if (ke.getKeyCode() == 40) { //Down
-            this.py += 10;
+         if(ke.getKeyCode() == 32){
+            lb.setText("");
         }
-        repaint();
+        else{
+            lb.setText(lb.getText() + ke.getKeyChar());
+        }
     }
 
     public void keyReleased(KeyEvent ke) {
