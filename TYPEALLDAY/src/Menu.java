17d25bb0f9374e66766e1b8ec1b6180c12e04588
thanapon072 <42941804+thanapon072@users.noreply.github.com>
2019-11-21 20:07:57 +0700

    Update Menu.java

--- TYPEALLDAY/src/Menu.java
+++ TYPEALLDAY/src/Menu.java
@@ -1,13 +1,6 @@
 
-import java.awt.FlowLayout;
-import java.awt.GridLayout;
-import java.awt.Toolkit;
-import javax.swing.ImageIcon;
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextField;
+import java.awt.*;
+import javax.swing.*;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
@@ -25,35 +18,46 @@ public class Menu extends JPanel {
     static int ySize = ((int) tk.getScreenSize().getHeight());
     private JFrame fr;
     private JPanel p1, p2, p3;
-    private ImageIcon i1, i2, i3;
-    private JLabel lb1;
+    private ImageIcon i1, i2, i3, bg;
+    private JLabel lb1, logo, lb_bg;
     private JButton btn1, btn2, btn3, btn4;
 
     public Menu() {
         fr = new JFrame("TypeAllDay");
-        p1 = new JPanel();
+
         p2 = new JPanel();
         p3 = new JPanel();
-        
-        lb1 = new JLabel(i1);
 
+        lb1 = new JLabel(i1);
+        bg = new ImageIcon("src/images/bg_2.jpg");
         i1 = new ImageIcon("src/images/logo.png");
         i2 = new ImageIcon("src/images/button1.png");
         i3 = new ImageIcon("src/images/button2.png");
-
+        p1 = new JPanel();
+        fr.setSize(500, 630);
+        lb_bg = new JLabel(bg);
+        logo = new JLabel(i1);
         btn1 = new JButton(i2);
         btn2 = new JButton(i3);
         btn3 = new JButton("Add");
-        fr.add(lb1);
+
+        p1.add(logo);
+        p1.add(btn1);
+        p1.add(btn2);
+        fr.add(lb_bg);
+        
+        //fr.add(p1);
         fr.setLayout(new FlowLayout());
-        fr.add(btn1);
-        fr.add(btn2);
         fr.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        fr.pack();
         fr.setLocationRelativeTo(null);
         fr.setVisible(true);
     }
-public static void main(String[] args) {
+
+    public void paintComponent(Graphics g) {
+
+    }
+
+    public static void main(String[] args) {
         new Menu();
     }
 }
